55ca9d8098709fdcf21c26d71429ed531f599036
ebru isleyen <ebruisleyene@gmail.com>
2023-04-15 05:02:45 +0300

    düzenleme

--- main.py
+++ main.py
@@ -6,17 +6,23 @@ url="https://www.kariyer.net/is-ilanlari/ankara-yazilim+test+uzmani?ct=6&pst=308
 baglan = requests.get(url).content
 parser=BeautifulSoup(baglan,"html.parser")
 kod=parser.find("div",{"class":"list-items-wrapper"}).find_all("div",{"class":"list-items"})
-
+sayac=0
 for i in kod:
   try:
     Job_Posting_Name =i.find("a",{"class":"k-ad-card"}).find("div",{"class":"card-top"}).find("div",{"class":"title-left"}).find("span",{"class":"k-ad-card-title multiline"}).text
-    print("Job Posting Name: ", Job_Posting_Name)
+    print("job posting name",Job_Posting_Name)
+    city=i.find("div",{"class":"job-detail"}).next.strip()
+    print("City",city)
+    workplace=i.find("div",{"class":"job-detail"}).find("span",{"class":"dot"}).next.strip()
+    print("Work Place",workplace)
+    
+    link = i.find("a",{"class":"k-ad-card"}).text
+    link=i.next.attrs["href"]
+    print("Link",link)
     
   # eger hata verirse, o veri cekmiyoruz ve devam ediyoruz.
   except AttributeError:
     continue
     
-for i in kod:
-  Job_Advertisement=i.find("div",{"class":"title-wrapper"}).text
-  print("job Advertisement: ", Job_Advertisement)
+
   
