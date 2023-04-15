18a254876e7abda89e9ffcac12ee737ea7164f6c
ebru isleyen <ebruisleyene@gmail.com>
2023-04-16 02:58:11 +0300

    dictnioary oluşturma

---
+++ deneme.py
@@ -0,0 +1,2 @@
+with open("data.json", "r",encoding="utf-8") as outfile:
+    print(outfile)
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 
-url="https://www.kariyer.net/is-ilanlari/ankara-yazilim+test+uzmani?ct=6&pst=3087&pkw=Yazilim%20Test%20Uzmani"
+url = "https://www.kariyer.net/is-ilanlari/ankara-yazilim+test+uzmani?ct=6&pst=3087&pkw=Yazilim%20Test%20Uzmani"
 baglan = requests.get(url).content
-parser=BeautifulSoup(baglan,"html.parser")
-kod=parser.find("div",{"class":"list-items-wrapper"}).find_all("div",{"class":"list-items"})
-sayac=0
+parser = BeautifulSoup(baglan,"html.parser")
+kod = parser.find("div",{"class":"list-items-wrapper"}).find_all("div",{"class":"list-items"})
+veriler=[]
 for i in kod:
   try:
-    Job_Posting_Name =i.find("a",{"class":"k-ad-card"}).find("div",{"class":"card-top"}).find("div",{"class":"title-left"}).find("span",{"class":"k-ad-card-title multiline"}).text
-    print("job posting name",Job_Posting_Name)
-    city=i.find("div",{"class":"job-detail"}).next.strip()
-    print("City",city)
-    workplace=i.find("div",{"class":"job-detail"}).find("span",{"class":"dot"}).next.strip()
-    print("Work Place",workplace)
+    title = i.find("a",{"class":"k-ad-card"}).find("div",{"class":"card-top"}).find("div",{"class":"title-left"}).find("span",{"class":"k-ad-card-title multiline"}).text
+    city = i.find("div",{"class":"job-detail"}).next.strip()
+    workplace = i.find("div",{"class":"job-detail"}).find("span",{"class":"dot"}).next.strip()
     
-    link = i.find("a",{"class":"k-ad-card"}).text
-    link=i.next.attrs["href"]
-    print("Link",link)
-    
+    link = "kariyer.net" + i.next.attrs["href"]
+  
+    job_post = {"title": title,"city":city,"workplace":workplace,"link":link,"sms":False}
+    veriler.append(job_post)
+       
   # eger hata verirse, o veri cekmiyoruz ve devam ediyoruz.
   except AttributeError:
     continue
-    
+
+with open("data.json", "a",encoding="utf-8") as outfile:
+    json.dump(veriler, outfile,indent=2,ensure_ascii=False)
 
   
