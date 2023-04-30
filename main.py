43b6af7aad254245b10eb559095e075a4cf58e9f
ebru isleyen <ebruisleyene@gmail.com>
2023-05-01 00:47:06 +0300

    son filtreleme

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import json
 from twilio.rest import Client
 with open("mycredentials.json")  as f:
   my_crendentials = json.load(f)
-  
+#accond_sid ve auth_token'u mycrendentials.json dosyasından çekiyoruz 
 account_sid = my_crendentials["account_sid"]
 auth_token = my_crendentials["auth_token"]
 client = Client(account_sid, auth_token)
-
+#daha önce mesaj atılmış iş ilanlarının dosya okuma işlemi
 atilmis_isler=[]
 try:
   with open("atilmis_is_ilanları.json",encoding="utf8") as file:
@@ -18,7 +18,7 @@ try:
       atilmis_isler = json.loads(veri)
 except (FileNotFoundError, json.JSONDecodeError):
   pass
-
+#beautifulsoupdan veri çekme ve (yeni_is_ilanlari[])'na ekleme
 url = "https://www.kariyer.net/is-ilanlari/ankara-yazilim+test+uzmani?ct=6&pst=3087&pkw=Yazilim%20Test%20Uzmani"
 baglan = requests.get(url).content
 parser = BeautifulSoup(baglan,"html.parser")
@@ -39,9 +39,9 @@ for i in kod:
 
   except AttributeError:
     continue
-#
+
 count = 0
-# ıs ılanları 
+# yeni _is ilanı atilmis_is ilanının içinde kontrol etme
 for yeni_is in yeni_is_ilanlari:
   
   link_var = False
@@ -51,10 +51,10 @@ for yeni_is in yeni_is_ilanlari:
        break
   if link_var:
     print("daha önce bu iş ilanı göndreildi.")
-    print(yeni_is["link"])
+    
   else:
     count +=1
-    # eger ıkı mesaj atılmıssa dursun
+    # count mesaj sınırlaması
     if count > 2:
       break
     print("bu iş ilanı yeni. mesaj at")
@@ -62,7 +62,7 @@ for yeni_is in yeni_is_ilanlari:
     whatsapp_mesaj = yeni_is["title"]+"\n"+yeni_is["city"]+"\n"+yeni_is["workplace"]+"\n"+yeni_is["link"]
     print(whatsapp_mesaj)
     
-    # whatapp?mesaj mesaj olarak gonderılıyor
+    #twilio kütüphanesini kullanarak (whatapp_mesaj) mesaj olarak gonderılıyor
     message = client.messages.create(
       from_='whatsapp:+14155238886',
       body=whatsapp_mesaj,
@@ -71,7 +71,7 @@ for yeni_is in yeni_is_ilanlari:
 
     atilmis_isler.append(yeni_is)
 
-  
+#(atilmis _is_ilanları)'na ekleme  
 with open("atilmis_is_ilanları.json","w",encoding="utf-8") as outfile:
     json.dump(atilmis_isler, outfile,indent=2,ensure_ascii=False)
 
@@ -80,18 +80,6 @@ with open("atilmis_is_ilanları.json","w",encoding="utf-8") as outfile:
        
        
 
-
-
-
-
-
-# with open("data.json", "w",encoding="utf-8") as outfile:
-#     json.dump(veriler, outfile,indent=2,ensure_ascii=False)
-    
-
-# infile =open("data.json","r",encoding="utf-8")
-
-# data=infile.readlines()
 
 
 
